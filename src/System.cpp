27984311d48275313d432efbfd1eb6c7af5532e4
Kidhanis De Jesus <kidhanis@mit.edu>
2014-01-30 19:24:56 -0500

    Added Runge-Kutta variation into System

--- src/System.cpp
+++ src/System.cpp
@@ -30,24 +30,41 @@ namespace nbody {
   }
 
   void System::integrateSystem( float dt ) {
-    Vector3f r, v, a;
-    for( size_t i = 0; i < _nBodies; ++i ) {
-      r = _body[i].position();
-      v = _body[i].velocity();
-      a = _body[i].force();
+	  for (size_t f = 0; f < _nBodies; ++f){
+		  Vector3f k1v, k2v, k3v, k4v, k1r, k2r, k3r, k4r, v, r;
+		  v = _body[f].velocity();
+		  r = _body[f].position();
 
-      v = v + ( a * dt );
-      v = v * _dampingFactor;
-      r = r + v * dt;
-
-      _body[i].position() = r;
-      _body[i].velocity() = v;
-    }
+		  k1r = dt*v;
+		  k1v = dt*grav(r, f);
+		  k2r = dt*(v + k1v / 2.0f);
+		  k2v = dt*(grav(r + k1r / 2.0f, f));
+		  k3r = dt*(v + k2v / 2.0f);
+		  k3v = dt*(grav(r + k2r / 2.0f, f));
+		  k4r = dt*(v + k3v);
+		  k4v = dt*(grav(r + k3r, f));
+		  v = v + (k1v + 2.0f * k2v + 2.0f * k3v + k4v) / 6.0f;
+		  r = r + (k1r + 2.0f * k2r + 2.0f * k3r + k4r) / 6.0f;
+		  _body[f].position() = r;
+		  _body[f].velocity() = v;
+	  }
   }
-
-  void System::update( float dt ) {
-    computeGravitation();
-    integrateSystem( dt );
+  Vector3f System::grav(const Vector3f x, size_t i){
+	  Vector3f forces{ 0.0f, 0.0f, 0.0f };
+	  for (size_t j = 0; j < _nBodies; ++j){
+		  if (i != j){
+			  Vector3f r = _body[j].position() - x;
+			  float distance = r.norm() + _softFactor;
+			  float invDist = 1.0f / distance;
+			  float invDistCubed = cube(invDist);
+			  forces = forces + NEWTON_G * _body[j].mass() * invDistCubed * r;
+		  }
+	  }
+	  return forces;
+  }
+  void System::update(float dt) {
+	  computeGravitation();
+	  integrateSystem(dt);
   }
 
   void System::readState( std::istream &input ) {
@@ -62,9 +79,9 @@ namespace nbody {
   }
 
   void System::writeState( std::ostream &output ) const {
-    output << _nBodies << "\n";
+    output << _nBodies << "\r\n";
     for( size_t i = 0; i < _nBodies; ++i ) {
-      output << _body[i] << "\n";
+      output << _body[i] << "\r\n";
     }
   }
 
